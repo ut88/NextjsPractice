3d4f8872c1003977c9b38f1cb7bb42e3a95e65c6
Utkarsh Badoni <utkarshbadoni2000@gmail.com>
2023-07-05 17:18:18 +0530

    Link redirect of web pages and dynamic values

--- pages/aboutus/[above4].js
+++ pages/aboutus/[above4].js
@@ -1,5 +1,7 @@
-
+import { useRouter } from "next/router";
 const Developer4=()=>{
+    const route=useRouter()
+    console.log(route.query.above4)
     return <h1>Developer doesn't exist</h1>
 }
 
--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,6 +1,11 @@
-
+import { Fragment } from "react";
+import Link from 'next/link';
 const Developer=()=>{
-    return <h1>Developer Page</h1>
+    return (<Fragment><h1>Developer Page</h1>
+    <h2><Link href="/aboutus/1">Yash</Link></h2>
+    <h2><Link href="/aboutus/2">Vaibhav</Link></h2>
+    <h2><Link href="/aboutus/3">Suresh</Link></h2>
+    </Fragment>)
 }
 
 export default Developer;
